1510ce0e0bc8569a5a46ce61f22159275cbb692d
AlexGh27 <alexghioca27@gmail.com>
2022-07-28 19:25:27 +0300

    Buttons ready, screen ready

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let output = 'car';
+document.getElementById('big-screen').innerHTML = output;
+
+function clear
+
+
+
+
+function operate (a, o, b) {
+
+    if (o === '+') {
+        return a + b;
+    }
+    else if (o === '-') {
+        return a - b;
+    }
+    else if (o === '*') {
+        return a * b;
+    }
+    else if (o === '/') {
+        return a / b;
+    }
+}
